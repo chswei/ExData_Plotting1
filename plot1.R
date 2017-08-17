20caa0212bc1160c515fd2f739206d447ec79cce
chswei <v09029w@gmail.com>
2017-08-17 16:54:42 +0800

    Finished plot 1

---
+++ plot1.R
@@ -0,0 +1,13 @@
+url <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+download.file(url, destfile = "dataset.zip")
+upzip("dataset.zip", exdir = "./ExData_Plotting1")
+data <- read.table("./ExData_Plotting1/household_power_consumption.txt", 
+                   header = T, sep = ";", na.strings = "?", skip = 66636, 
+                   nrows = 2880, comment.char = "")
+colnames(data) <- c("Date", "Time", "Global_active_power", 
+                    "Global_reactive_power", "Voltage", "Global_intensity", 
+                    "Sub_metering_1", "Sub_metering_2", "Sub_metering_3")
+data$Date <- as.Date(data$Date, format = "%d/%m/%Y")
+data$Time <- strptime(data$Time, format = "%H:%M:%S")
+hist(data$Global_active_power, col = "red", 
+     xlab = "Global Active Power (kilowatts)", main = "Global Active Power")
