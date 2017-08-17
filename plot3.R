32600db34bc5e75ffce8eeae6ac6bc42faaeee0f
chswei <v09029w@gmail.com>
2017-08-17 22:42:57 +0800

    Finished plot 3

--- plot3.R
+++ plot3.R
@@ -9,7 +9,12 @@ colnames(data) <- c("Date", "Time", "Global_active_power",
                     "Sub_metering_1", "Sub_metering_2", "Sub_metering_3")
 data$Date_Time <- paste(data$Date, data$Time)
 data$Date_Time <- strptime(data$Date_Time, format = "%d/%m/%Y %H:%M:%S")
-png("./ExData_Plotting1/plot2.png")
-with(data, plot(Date_Time, Global_active_power, type = "l", 
-     xlab = "", ylab = "Global Active Power (kilowatts)"))
+png("./ExData_Plotting1/plot3.png")
+with(data, plot(Date_Time, Sub_metering_1, type = "n", 
+                xlab = "", ylab = "Energy sub metering"))
+lines(data$Date_Time, data$Sub_metering_1, col = "black")
+lines(data$Date_Time, data$Sub_metering_2, col = "red")
+lines(data$Date_Time, data$Sub_metering_3, col = "blue")
+legend("topright", lwd = c(1, 1, 1), col = c("black", "red", "blue"), 
+       legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
 dev.off()
